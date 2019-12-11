29ff262a25b58b72ebf3aa39a030131522466515
小白 <xiaobai@xiaobaideMacBook-Pro.local>
2019-12-11 17:43:18 +0800

    add backup process

---
+++ export_sync.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# set debug mode
+set -x
+
+# set docker name
+if [[ "empty${1}" == "empty" ]]; then
+    MONGO_DOCKER_NAME=vj4-docker_mongodb_1
+else
+    MONGO_DOCKER_NAME=${1}
+fi
+
+# set docker mapping directory
+BACKUP_DIR=backup
+
+# string for today
+TODAY=$(date +%Y%m%d)
+
+# get script's location path
+CURRENT_PATH=$(cd $(dirname $0) && pwd)
+
+# drop directory if exists
+rm -rf ${CURRENT_PATH}/${BACKUP_DIR}/${TODAY}
+
+# make a directory in docker mapping directory
+mkdir -p ${CURRENT_PATH}/${BACKUP_DIR}/${TODAY}
+
+# export mongo data to directory made just now
+docker exec -it ${MONGO_DOCKER_NAME} sh -c "mongodump --db vijos4 --out /backup/${TODAY}"
+
+# zip folder
+tar -C ${CURRENT_PATH}/${BACKUP_DIR} -czvf ${CURRENT_PATH}/${BACKUP_DIR}/${TODAY}.tar.gz ${TODAY}
+
+# sync data to remote using rsync
+/usr/bin/rsync -rtvzc --progress --password-file=/etc/client.pass ${CURRENT_PATH}/${BACKUP_DIR}/${TODAY}.tar.gz xg@172.18.45.238::lyg
+
+# only keep last 7 day's data
+DAYS=7
+for ((i=${DAYS} + 30; i > ${DAYS}; i--)); do
+    DT=$(eval "date +'%Y%m%d' -d'${i} days ago'")
+    rm -rf ${CURRENT_PATH}/${BACKUP_DIR}/${DT}
+    rm -rf ${CURRENT_PATH}/${BACKUP_DIR}/${DT}.tar.gz
+done
---
+++ import_clear.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# set debug mode
+set -x
+
+# set docker name
+if [[ "empty${1}" == "empty" ]]; then
+    MONGO_DOCKER_NAME=vj4-docker_mongodb_1
+else
+    MONGO_DOCKER_NAME=${1}
+fi
+
+# set docker mapping directory
+BACKUP_DIR=backup
+
+# string for today
+TODAY=$(date +%Y%m%d)
+
+# get script's location path
+CURRENT_PATH=$(cd $(dirname $0) && pwd)
+
+# check whether file exist
+if [[ ! -f ${CURRENT_PATH}/${BACKUP_DIR}/${TODAY}.tar.gz ]]; then
+    exit 0
+fi
+
+# unzip file to folder
+tar -xzvf ${CURRENT_PATH}/${BACKUP_DIR}/${TODAY}.tar.gz -C ${CURRENT_PATH}/${BACKUP_DIR}
+
+# import data to mongo
+docker exec -it ${MONGO_DOCKER_NAME} sh -c "mongorestore --db vijos4 /backup/${TODAY}/vijos4"
+
+
+# only keep last 7 day's data
+DAYS=7
+for ((i=${DAYS} + 30; i > ${DAYS}; i--)); do
+    DT=$(eval "date +'%Y%m%d' -d'${i} days ago'")
+    rm -rf ${CURRENT_PATH}/${BACKUP_DIR}/${DT}
+    rm -rf ${CURRENT_PATH}/${BACKUP_DIR}/${DT}.tar.gz
+done
